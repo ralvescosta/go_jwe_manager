cbac3b044999f14882329f40491b6ca042f422ef
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-16 06:58:09 -0300

    feat: view model

--- pkg/interfaces/http/view_models/create_key_viewmodel.go
+++ pkg/interfaces/http/view_models/create_key_viewmodel.go
@@ -1,5 +1,13 @@
 package viewmodels
 
+import "jwemanager/pkg/domain/dtos"
+
 type CreateKeyViewModel struct {
 	Id string `json:"id" validate:"required, uuidv4"`
+}
+
+func (pst CreateKeyViewModel) ToDto() dtos.Key {
+	return dtos.Key{
+		Id: pst.Id,
+	}
 }
