5dd0e5fc2d0a815a62f1b50427cc681adc8044f9
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-17 04:21:32 -0300

    refactor: cmd

--- cmd/http_cmd.go
+++ cmd/http_cmd.go
@@ -1,1 +1,21 @@
 package cmd
+
+import "jwemanager/pkg/infra/environments"
+
+func HttpServer() error {
+	err := environments.Configure()
+	if err != nil {
+		return err
+	}
+
+	container := NewContainer()
+	container.httpServer.Setup()
+
+	container.keysRoutes.Register(container.httpServer)
+
+	if err := container.httpServer.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
--- cmd/http_ioc.go
+++ cmd/http_ioc.go
@@ -1,1 +1,33 @@
 package cmd
+
+import (
+	"jwemanager/pkg/app/interfaces"
+	"jwemanager/pkg/app/usecases"
+	httpServer "jwemanager/pkg/infra/http_server"
+	"jwemanager/pkg/infra/logger"
+	"jwemanager/pkg/interfaces/http/handlers"
+	"jwemanager/pkg/interfaces/http/presenters"
+)
+
+type webApiContainer struct {
+	logger     interfaces.ILogger
+	httpServer httpServer.IHttpServer
+
+	keysRoutes presenters.IKeysRoutes
+}
+
+func NewContainer() webApiContainer {
+	logger := logger.NewLogger()
+	httpServer := httpServer.NewHttpServer(logger)
+
+	createKeyUseCase := usecases.NewCreateKeyUseCase()
+	keyHandlers := handlers.NewKeysHandlers(logger, struct{}{}, createKeyUseCase)
+	keysRoutes := presenters.NewKeysRoutes(logger, keyHandlers)
+
+	return webApiContainer{
+		logger,
+		httpServer,
+
+		keysRoutes,
+	}
+}
