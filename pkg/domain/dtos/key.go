051bf477057c882a006bfb2870278d14aaea6282
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-17 05:14:41 -0300

    refactory: dto

--- pkg/domain/dtos/key.go
+++ pkg/domain/dtos/key.go
@@ -3,7 +3,9 @@ package dtos
 import "time"
 
 type Key struct {
-	Id        string
+	ID        string
+	UserID    string
+	KeyID     string
 	PubKey    string
 	PriKey    string
 	CreatedAt time.Time
