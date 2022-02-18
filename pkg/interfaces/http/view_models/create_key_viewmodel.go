2526ad9e9aac266468c8d2638109d7a6dee9eae8
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-18 05:32:00 -0300

    refactor: viewmodel

--- pkg/interfaces/http/view_models/create_key_viewmodel.go
+++ pkg/interfaces/http/view_models/create_key_viewmodel.go
@@ -1,15 +1,34 @@
 package viewmodels
 
-import "jwemanager/pkg/domain/dtos"
+import (
+	"jwemanager/pkg/domain/dtos"
+	"time"
+)
 
 type CreateKeyViewModel struct {
-	UserID string `json:"user_id" validate:"required, uuidv4"`
-	KeyID  string `json:"key_id" validate:"required, uuidv4"`
+	UserID string `json:"user_id" validate:"required,uuid4"`
+	KeyID  string `json:"key_id" validate:"required,uuid4"`
+}
+
+type CreatedKeyViewModel struct {
+	UserID    string `json:"user_id"`
+	KeyID     string `json:"key_id"`
+	PubKey    string `json:"pub_key"`
+	ExpiredAt string `json:"expired_at"`
 }
 
 func (pst CreateKeyViewModel) ToDto() dtos.Key {
 	return dtos.Key{
 		UserID: pst.UserID,
 		KeyID:  pst.KeyID,
+	}
+}
+
+func NewCreatedKeyViewModel(key dtos.Key) CreatedKeyViewModel {
+	return CreatedKeyViewModel{
+		UserID:    key.UserID,
+		KeyID:     key.KeyID,
+		PubKey:    key.PubKey,
+		ExpiredAt: key.ExpiredAt.Format(time.RFC3339),
 	}
 }
