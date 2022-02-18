06785395a066dd516886c61bf6b53c494df76ab1
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-18 05:32:27 -0300

    refactor: usecase

--- pkg/app/usecases/create_key_usecase.go
+++ pkg/app/usecases/create_key_usecase.go
@@ -1,16 +1,32 @@
 package usecases
 
 import (
+	"context"
+	"jwemanager/pkg/app/interfaces"
 	"jwemanager/pkg/domain/dtos"
 	"jwemanager/pkg/domain/usecases"
+	"time"
 )
 
-type createKeyUseCase struct{}
-
-func (createKeyUseCase) Execute(key dtos.Key) (dtos.Key, error) {
-	return dtos.Key{}, nil
+type createKeyUseCase struct {
+	logger  interfaces.ILogger
+	keyRepo interfaces.IKeyRepository
 }
 
-func NewCreateKeyUseCase() usecases.ICreateKeyUseCase {
-	return createKeyUseCase{}
+func (pst createKeyUseCase) Execute(ctx context.Context, key dtos.Key) (dtos.Key, error) {
+	key.ExpiredAt = time.Now().Add(time.Hour * 720)
+
+	result, err := pst.keyRepo.CreateKey(ctx, key)
+	if err != nil {
+		return dtos.Key{}, err
+	}
+
+	return result, nil
+}
+
+func NewCreateKeyUseCase(
+	logger interfaces.ILogger,
+	keyRepo interfaces.IKeyRepository,
+) usecases.ICreateKeyUseCase {
+	return createKeyUseCase{logger, keyRepo}
 }
