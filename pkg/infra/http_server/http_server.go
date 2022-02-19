3f75c496fe9bfefe2e24b5c4372eed5c45e95a5a
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-19 06:01:28 -0300

    refactor: http server

--- pkg/infra/http_server/http_server.go
+++ pkg/infra/http_server/http_server.go
@@ -17,31 +17,29 @@ type IHttpServer interface {
 }
 
 type HttpServer struct {
-	server *gin.Engine
+	env    interfaces.IEnvironments
 	logger interfaces.ILogger
+	server *gin.Engine
 }
 
 var httpServerWrapper = gin.New
 
-func (hs *HttpServer) Setup() {
-	hs.server = httpServerWrapper()
-	hs.server.Use(GinLogger(hs.logger))
+func (pst *HttpServer) Setup() {
+	pst.server = httpServerWrapper()
+	pst.server.Use(GinLogger(pst.logger))
+	pst.server.SetTrustedProxies(nil)
 }
 
 func (hs HttpServer) RegistreRoute(method string, path string, handlers ...gin.HandlerFunc) error {
 	switch method {
 	case "POST":
 		hs.server.POST(path, handlers...)
-
 	case "GET":
 		hs.server.GET(path, handlers...)
-
 	case "PUT":
 		hs.server.PUT(path, handlers...)
-
 	case "PATCH":
 		hs.server.PATCH(path, handlers...)
-
 	case "DELETE":
 		hs.server.DELETE(path, handlers...)
 	default:
@@ -50,15 +48,23 @@ func (hs HttpServer) RegistreRoute(method string, path string, handlers ...gin.H
 	return nil
 }
 
-func (hs HttpServer) Run() error {
+func (pst HttpServer) Run() error {
 	host := os.Getenv("HOST")
 	port := os.Getenv("PORT")
+	url := fmt.Sprintf("%s:%s", host, port)
 
-	return hs.server.Run(fmt.Sprintf("%s:%s", host, port))
+	if pst.env.GO_ENV() != pst.env.PROD_ENV() {
+		certPath := os.Getenv("TLS_CERT_PATH")
+		keyPath := os.Getenv("TLS_KEY_PATH")
+		return pst.server.RunTLS(url, certPath, keyPath)
+	}
+
+	return pst.server.Run(url)
 }
 
-func NewHttpServer(logger interfaces.ILogger) IHttpServer {
+func NewHttpServer(environments interfaces.IEnvironments, logger interfaces.ILogger) IHttpServer {
 	return &HttpServer{
+		env:    environments,
 		logger: logger,
 	}
 }
