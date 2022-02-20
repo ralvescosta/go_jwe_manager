e239a535908cc982bde54903cd16abf6164400d6
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-20 14:00:26 -0300

    refactor: redis env

--- pkg/infra/database/database.go
+++ pkg/infra/database/database.go
@@ -3,32 +3,49 @@ package database
 import (
 	"context"
 	"fmt"
-	"jwemanager/pkg/app/interfaces"
+	"os"
+	"strconv"
 	"time"
+
+	"jwemanager/pkg/app/interfaces"
 
 	"github.com/go-redis/redis/v8"
 )
 
 func Connection(logger interfaces.ILogger, shotdown chan bool) (*redis.Client, error) {
+	host := os.Getenv("REDIS_HOST")
+	port := os.Getenv("REDIS_PORT")
+	password := os.Getenv("REDIS_PASSWORD")
+
+	db, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+	if err != nil {
+		return nil, err
+	}
+
+	secondsToSleep, err := strconv.Atoi(os.Getenv("REDIS_SECONDS_TO_PING"))
+	if err != nil {
+		return nil, err
+	}
+
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
+		Addr:     fmt.Sprintf("%s:%s", host, port),
+		Password: password,
+		DB:       db,
 	})
 
-	_, err := rdb.Ping(context.Background()).Result()
+	_, err = rdb.Ping(context.Background()).Result()
 	if err != nil {
 		logger.Error(fmt.Sprintf("[Database::Connection] - Redis Connection failure : %s", err.Error()))
 		return nil, err
 	}
 
-	go signalShotdown(rdb, logger, shotdown)
+	go signalShotdown(rdb, logger, secondsToSleep, shotdown)
 
 	return rdb, nil
 }
 
-func signalShotdown(rdb *redis.Client, logger interfaces.ILogger, shotdown chan bool) {
-	time.Sleep(time.Second * 10)
+func signalShotdown(rdb *redis.Client, logger interfaces.ILogger, secondsToSleep int, shotdown chan bool) {
+	time.Sleep(time.Duration(secondsToSleep) * time.Second)
 	_, err := rdb.Ping(context.Background()).Result()
 	if err != nil {
 		logger.Error(fmt.Sprintf("[Database::Connection] - Redis Connection failure : %s", err.Error()))
