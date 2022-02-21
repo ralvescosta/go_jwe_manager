573d53e8c806066ec2a5797d1ab2dc270953182c
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-21 07:54:40 -0300

    feat: encrypt  viewmodel

---
+++ pkg/interfaces/http/view_models/encrypt_viewmodel.go
@@ -0,0 +1,26 @@
+package viewmodels
+
+import valueObjects "jwemanager/pkg/domain/value_objects"
+
+type EncryptViewModel struct {
+	UserID string                 `json:"user_id" validate:"required,uuid4"`
+	KeyID  string                 `json:"key_id" validate:"required,uuid4"`
+	Data   map[string]interface{} `json:"data" validate:"required"`
+}
+type EncryptedViewModel struct {
+	EncryptedData string `json:"encrypted_data"`
+}
+
+func (pst EncryptViewModel) ToValueObject() valueObjects.EncryptValueObject {
+	return valueObjects.EncryptValueObject{
+		UserID: pst.UserID,
+		KeyID:  pst.KeyID,
+		Data:   pst.Data,
+	}
+}
+
+func ToEncryptedViewModel(vo valueObjects.EncryptedValueObject) EncryptedViewModel {
+	return EncryptedViewModel{
+		EncryptedData: vo.EncryptedData,
+	}
+}
