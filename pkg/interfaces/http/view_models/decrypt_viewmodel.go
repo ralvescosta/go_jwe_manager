cc37965986357717c2cfb919221380f73dcb0d3b
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-21 08:17:28 -0300

    feat: decrypt viewmodel

---
+++ pkg/interfaces/http/view_models/decrypt_viewmodel.go
@@ -0,0 +1,27 @@
+package viewmodels
+
+import valueObjects "jwemanager/pkg/domain/value_objects"
+
+type DecryptViewModel struct {
+	UserID        string `json:"user_id" validate:"required,uuid4"`
+	KeyID         string `json:"key_id" validate:"required,uuid4"`
+	EncryptedData string `json:"encrypted_data" validate:"required"`
+}
+
+type DecryptedViewModel struct {
+	Data map[string]interface{} `json:"data"`
+}
+
+func (pst DecryptViewModel) ToValueObject() valueObjects.DecryptValueObject {
+	return valueObjects.DecryptValueObject{
+		UserID:        pst.UserID,
+		KeyID:         pst.KeyID,
+		EncryptedData: []byte(pst.EncryptedData),
+	}
+}
+
+func ToDecryptedViewModel(vo valueObjects.DecryptedValueObject) DecryptedViewModel {
+	return DecryptedViewModel{
+		Data: vo.Data,
+	}
+}
