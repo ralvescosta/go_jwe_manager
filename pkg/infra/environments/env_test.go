97d7c1170f284e9e7e2c786c7e18463cee942fd6
ralvescosta <rafael.rac.mg@gmail.com>
2022-03-01 20:38:44 -0300

    test: improve env

--- pkg/infra/environments/env_test.go
+++ pkg/infra/environments/env_test.go
@@ -3,41 +3,80 @@ package environments
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_Configure_Env_Without_GoEnv(t *testing.T) {
-	os.Setenv("GO_ENV", "")
-	var envFile string
-	dotEnvConfig = func(arg string) error {
-		envFile = arg
-		return nil
-	}
+func Test_Configure(t *testing.T) {
+	t.Run("should configure development env when GO_ENV is undefined", func(t *testing.T) {
+		os.Setenv("GO_ENV", "")
+		var envFile string
+		dotEnvConfig = func(arg string) error {
+			envFile = arg
+			return nil
+		}
 
-	env := NewEnvironment()
-	err := env.Configure()
+		env := NewEnvironment()
+		err := env.Configure()
 
-	if err != nil {
-		t.Error("dotenv need to call only dotEnvConfig function")
-	}
-	if envFile != ".env.development" {
-		t.Error("dotenv need to call only dotEnvConfig function")
-	}
+		if err != nil {
+			t.Error("dotenv need to call only dotEnvConfig function")
+		}
+		if envFile != ".env.development" {
+			t.Error("dotenv need to call only dotEnvConfig function")
+		}
+	})
+
+	t.Run("should configure env using the GO_ENV", func(t *testing.T) {
+		os.Setenv("GO_ENV", "production")
+		var envFile string
+		dotEnvConfig = func(arg string) error {
+			envFile = arg
+			return nil
+		}
+		env := NewEnvironment()
+		err := env.Configure()
+
+		if err != nil {
+			t.Error("dotenv need to call only dotEnvConfig function")
+		}
+		if envFile != ".env.production" {
+			t.Error("dotenv need to call only dotEnvConfig function")
+		}
+	})
 }
 
-func Test_Configure_Env_With_GoEnv(t *testing.T) {
-	os.Setenv("GO_ENV", "production")
-	var envFile string
-	dotEnvConfig = func(arg string) error {
-		envFile = arg
-		return nil
-	}
-	env := NewEnvironment()
-	err := env.Configure()
+func Test_GO_ENV(t *testing.T) {
+	t.Run("should return the config", func(t *testing.T) {
+		goEnv := "development"
+		os.Setenv("GO_ENV", goEnv)
 
-	if err != nil {
-		t.Error("dotenv need to call only dotEnvConfig function")
-	}
-	if envFile != ".env.production" {
-		t.Error("dotenv need to call only dotEnvConfig function")
-	}
+		env := NewEnvironment()
+
+		assert.Equal(t, goEnv, env.GO_ENV())
+	})
+}
+
+func Test_DEV_ENV(t *testing.T) {
+	t.Run("should return the config", func(t *testing.T) {
+		env := NewEnvironment()
+
+		assert.Equal(t, "development", env.DEV_ENV())
+	})
+}
+
+func Test_STAGING_ENV(t *testing.T) {
+	t.Run("should return the config", func(t *testing.T) {
+		env := NewEnvironment()
+
+		assert.Equal(t, "staging", env.STAGING_ENV())
+	})
+}
+
+func Test_PROD_ENV(t *testing.T) {
+	t.Run("should return the config", func(t *testing.T) {
+		env := NewEnvironment()
+
+		assert.Equal(t, "production", env.PROD_ENV())
+	})
 }
