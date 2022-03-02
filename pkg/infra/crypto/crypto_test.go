003d231e9a292998d4d3316779934b44c68869dc
ralvescosta <rafael.rac.mg@gmail.com>
2022-03-02 04:48:09 -0300

    test: crypto test

--- pkg/infra/crypto/crypto_test.go
+++ pkg/infra/crypto/crypto_test.go
@@ -3,10 +3,13 @@ package crypto
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"errors"
 	"jwemanager/pkg/app/interfaces"
 	"jwemanager/pkg/infra/logger"
 	"testing"
 
+	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/lestrrat-go/jwx/jwe"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,6 +22,28 @@ func Test_Encrypt(t *testing.T) {
 		assert.NoError(t, err)
 		assert.NotNil(t, result)
 	})
+
+	t.Run("should return error if some error occur in marshal", func(t *testing.T) {
+		sut := makeCryptoSutRtn()
+
+		marshal = func(v interface{}) ([]byte, error) {
+			return nil, errors.New("some error")
+		}
+		_, err := sut.crypto.Encrypt(&sut.privateKeyMocked.PublicKey, nil)
+
+		assert.Error(t, err)
+	})
+
+	t.Run("should return error if some error occur in encrypt", func(t *testing.T) {
+		sut := makeCryptoSutRtn()
+
+		encrypt = func(payload []byte, keyalg jwa.KeyEncryptionAlgorithm, key interface{}, contentalg jwa.ContentEncryptionAlgorithm, compressalg jwa.CompressionAlgorithm, options ...jwe.EncryptOption) ([]byte, error) {
+			return nil, errors.New("some error")
+		}
+		_, err := sut.crypto.Encrypt(&sut.privateKeyMocked.PublicKey, nil)
+
+		assert.Error(t, err)
+	})
 }
 
 func Test_Decrypt(t *testing.T) {
@@ -30,6 +55,30 @@ func Test_Decrypt(t *testing.T) {
 
 		assert.NoError(t, err)
 		assert.Equal(t, sut.dataMocked, result)
+	})
+
+	t.Run("should error if some error occur in decrypt", func(t *testing.T) {
+		sut := makeCryptoSutRtn()
+		encrypted, _ := sut.crypto.Encrypt(&sut.privateKeyMocked.PublicKey, sut.dataMocked)
+
+		decrypt = func(buf []byte, alg jwa.KeyEncryptionAlgorithm, key interface{}, options ...jwe.DecryptOption) ([]byte, error) {
+			return nil, errors.New("some error")
+		}
+		_, err := sut.crypto.Decrypt(sut.privateKeyMocked, encrypted)
+
+		assert.Error(t, err)
+	})
+
+	t.Run("should error if some error occur in unmarshal", func(t *testing.T) {
+		sut := makeCryptoSutRtn()
+		encrypted, _ := sut.crypto.Encrypt(&sut.privateKeyMocked.PublicKey, sut.dataMocked)
+
+		decrypt = func(buf []byte, alg jwa.KeyEncryptionAlgorithm, key interface{}, options ...jwe.DecryptOption) ([]byte, error) {
+			return nil, nil
+		}
+		_, err := sut.crypto.Decrypt(sut.privateKeyMocked, encrypted)
+
+		assert.Error(t, err)
 	})
 }
 
