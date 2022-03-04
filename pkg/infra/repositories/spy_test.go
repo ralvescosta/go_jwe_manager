2ced09249efb4494e3e1f23bc98c19de36757c3d
ralvescosta <rafael.rac.mg@gmail.com>
2022-03-04 05:15:04 -0300

    feat: repository spy tests

--- pkg/infra/repositories/spy_test.go
+++ pkg/infra/repositories/spy_test.go
@@ -1,1 +1,41 @@
 package repositories
+
+import (
+	"context"
+	valueObjects "jwemanager/pkg/domain/value_objects"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CreateKey(t *testing.T) {
+	t.Run("execute correctly", func(t *testing.T) {
+		sut := NewKeyRepositorySpy()
+		ctx := context.Background()
+		key := valueObjects.Key{}
+		timeToExpiration := 0
+
+		sut.On("CreateKey", ctx, key, timeToExpiration).Return(valueObjects.Key{}, nil)
+
+		result, err := sut.CreateKey(ctx, key, timeToExpiration)
+
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+	})
+}
+
+func Test_GetKeyByID(t *testing.T) {
+	t.Run("execute correctly", func(t *testing.T) {
+		sut := NewKeyRepositorySpy()
+		ctx := context.Background()
+		userID := "some_user"
+		keyID := "some_key"
+
+		sut.On("GetKeyByID", ctx, userID, keyID).Return(valueObjects.Key{}, nil)
+
+		result, err := sut.GetKeyByID(ctx, userID, keyID)
+
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+	})
+}
