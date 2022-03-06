ceecdcdb8c390b51ebe68fc236493e288c2e4b47
ralvescosta <rafael.rac.mg@gmail.com>
2022-03-06 07:35:33 -0300

    refactor: crypt handler tsts

--- pkg/interfaces/http/handlers/crypto_handlers_test.go
+++ pkg/interfaces/http/handlers/crypto_handlers_test.go
@@ -23,10 +23,10 @@ func Test_Encrypt(t *testing.T) {
 	t.Run("should execute correctly", func(t *testing.T) {
 		sut := makeCryptoHandlerSut()
 
-		sut.validator.On("ValidateStruct", sut.model).Return([]valueObjects.ValidateResult(nil))
-		sut.encryptUseCase.On("Encrypt", sut.req.Ctx, sut.model.ToValueObject()).Return(valueObjects.EncryptedValueObject{}, nil)
+		sut.validator.On("ValidateStruct", sut.encryptModel).Return([]valueObjects.ValidateResult(nil))
+		sut.encryptUseCase.On("Encrypt", sut.encryptReq.Ctx, sut.encryptModel.ToValueObject()).Return(valueObjects.EncryptedValueObject{}, nil)
 
-		res := sut.handler.Encrypt(sut.req)
+		res := sut.handler.Encrypt(sut.encryptReq)
 
 		assert.Equal(t, http.StatusOK, res.StatusCode)
 		sut.validator.AssertExpectations(t)
@@ -36,19 +36,20 @@ func Test_Encrypt(t *testing.T) {
 	t.Run("should return badRequest status if some error in body parser", func(t *testing.T) {
 		sut := makeCryptoHandlerSut()
 
-		sut.req.Body = nil
+		sut.encryptReq.Body = nil
 
-		res := sut.handler.Encrypt(sut.req)
+		res := sut.handler.Encrypt(sut.encryptReq)
 
 		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
 	})
+
 	t.Run("should return badRequest if some validate error occur", func(t *testing.T) {
 		sut := makeCryptoHandlerSut()
 
-		sut.validator.On("ValidateStruct", sut.model).Return([]valueObjects.ValidateResult{{IsValid: true, Field: "some", Message: "some"}})
+		sut.validator.On("ValidateStruct", sut.encryptModel).Return([]valueObjects.ValidateResult{{IsValid: true, Field: "some", Message: "some"}})
 		sut.logger.On("Error", "some", []zapcore.Field(nil))
 
-		res := sut.handler.Encrypt(sut.req)
+		res := sut.handler.Encrypt(sut.encryptReq)
 
 		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
 		sut.validator.AssertExpectations(t)
@@ -57,20 +58,65 @@ func Test_Encrypt(t *testing.T) {
 	t.Run("should return internalServerError if usecase return internalError", func(t *testing.T) {
 		sut := makeCryptoHandlerSut()
 
-		sut.validator.On("ValidateStruct", sut.model).Return([]valueObjects.ValidateResult(nil))
-		sut.encryptUseCase.On("Encrypt", sut.req.Ctx, sut.model.ToValueObject()).Return(valueObjects.EncryptedValueObject{}, errors.NewInternalError("some error"))
+		sut.validator.On("ValidateStruct", sut.encryptModel).Return([]valueObjects.ValidateResult(nil))
+		sut.encryptUseCase.On("Encrypt", sut.encryptReq.Ctx, sut.encryptModel.ToValueObject()).Return(valueObjects.EncryptedValueObject{}, errors.NewInternalError("some error"))
 
-		res := sut.handler.Encrypt(sut.req)
+		res := sut.handler.Encrypt(sut.encryptReq)
 
 		assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
 		sut.validator.AssertExpectations(t)
 		sut.encryptUseCase.AssertExpectations(t)
 	})
-
 }
 
 func Test_Decrypt(t *testing.T) {
+	t.Run("should execute correctly", func(t *testing.T) {
+		sut := makeCryptoHandlerSut()
 
+		sut.validator.On("ValidateStruct", sut.decryptModel).Return([]valueObjects.ValidateResult(nil))
+		sut.decryptUseCase.On("Decrypt", sut.decryptReq.Ctx, sut.decryptModel.ToValueObject()).Return(valueObjects.DecryptedValueObject{}, nil)
+
+		res := sut.handler.Decrypt(sut.decryptReq)
+
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+		sut.validator.AssertExpectations(t)
+		sut.decryptUseCase.AssertExpectations(t)
+	})
+
+	t.Run("should return badRequest status if some error in body parser", func(t *testing.T) {
+		sut := makeCryptoHandlerSut()
+
+		sut.decryptReq.Body = nil
+
+		res := sut.handler.Decrypt(sut.decryptReq)
+
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+	})
+
+	t.Run("should return badRequest if some validate error occur", func(t *testing.T) {
+		sut := makeCryptoHandlerSut()
+
+		sut.validator.On("ValidateStruct", sut.decryptModel).Return([]valueObjects.ValidateResult{{IsValid: true, Field: "some", Message: "some"}})
+		sut.logger.On("Error", "some", []zapcore.Field(nil))
+
+		res := sut.handler.Decrypt(sut.decryptReq)
+
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+		sut.validator.AssertExpectations(t)
+	})
+
+	t.Run("should return internalServerError if usecase return internalError", func(t *testing.T) {
+		sut := makeCryptoHandlerSut()
+
+		sut.validator.On("ValidateStruct", sut.decryptModel).Return([]valueObjects.ValidateResult(nil))
+		sut.decryptUseCase.On("Decrypt", sut.decryptReq.Ctx, sut.decryptModel.ToValueObject()).Return(valueObjects.DecryptedValueObject{}, errors.NewInternalError("some error"))
+
+		res := sut.handler.Decrypt(sut.decryptReq)
+
+		assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+		sut.validator.AssertExpectations(t)
+		sut.decryptUseCase.AssertExpectations(t)
+	})
 }
 
 type cryptoHandlerSutRtn struct {
@@ -80,8 +126,10 @@ type cryptoHandlerSutRtn struct {
 	httpResFactory factories.HttpResponseFactory
 	encryptUseCase *usecases.EncryptUseCaseSpy
 	decryptUseCase *usecases.DecryptUseCaseSpy
-	model          vm.EncryptViewModel
-	req            httpServer.HttpRequest
+	encryptModel   vm.EncryptViewModel
+	encryptReq     httpServer.HttpRequest
+	decryptModel   vm.DecryptViewModel
+	decryptReq     httpServer.HttpRequest
 }
 
 func makeCryptoHandlerSut() cryptoHandlerSutRtn {
@@ -93,16 +141,27 @@ func makeCryptoHandlerSut() cryptoHandlerSutRtn {
 
 	handler := NewCryptHandler(logger, validator, httpResFactory, encryptUseCase, decryptUseCase)
 
-	model := vm.EncryptViewModel{
+	encryptModel := vm.EncryptViewModel{
 		UserID: "45a710fd-1bb4-4171-9571-224c241838e6",
 		KeyID:  "fcf7ee3e-9512-4416-8e1f-da12a667832a",
 		Data:   make(map[string]interface{}),
 	}
-	body, _ := json.Marshal(model)
-	req := httpServer.HttpRequest{
+	encryptBody, _ := json.Marshal(encryptModel)
+	encryptReq := httpServer.HttpRequest{
 		Ctx:  context.Background(),
-		Body: body,
+		Body: encryptBody,
 	}
 
-	return cryptoHandlerSutRtn{handler, logger, validator, httpResFactory, encryptUseCase, decryptUseCase, model, req}
+	decryptModel := vm.DecryptViewModel{
+		UserID:        "45a710fd-1bb4-4171-9571-224c241838e6",
+		KeyID:         "fcf7ee3e-9512-4416-8e1f-da12a667832a",
+		EncryptedData: "",
+	}
+	decryptBody, _ := json.Marshal(encryptModel)
+	decryptReq := httpServer.HttpRequest{
+		Ctx:  context.Background(),
+		Body: decryptBody,
+	}
+
+	return cryptoHandlerSutRtn{handler, logger, validator, httpResFactory, encryptUseCase, decryptUseCase, encryptModel, encryptReq, decryptModel, decryptReq}
 }
